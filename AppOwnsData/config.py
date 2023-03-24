f2f116c15176aa7de5288abdc5122c28add87a0b
muhtaromzain <muhtaromzain@gmail.com>
2023-03-24 16:26:40 +0700

    update config

--- AppOwnsData/config.py
+++ AppOwnsData/config.py
@@ -5,21 +5,6 @@ class BaseConfig(object):
 
     # Can be set to 'MasterUser' or 'ServicePrincipal'
     AUTHENTICATION_MODE = 'ServicePrincipal'
-
-    # Workspace Id in which the report is present
-    WORKSPACE_ID = ''
-    
-    # Report Id for which Embed token needs to be generated
-    REPORT_ID = ''
-    
-    # Id of the Azure tenant in which AAD app and Power BI report is hosted. Required only for ServicePrincipal authentication mode.
-    TENANT_ID = ''
-    
-    # Client Id (Application Id) of the AAD app
-    CLIENT_ID = ''
-    
-    # Client Secret (App Secret) of the AAD app. Required only for ServicePrincipal authentication mode.
-    CLIENT_SECRET = ''
     
     # Scope Base of AAD app. Use the below configuration to use all the permissions provided in the AAD app through Azure portal.
     SCOPE_BASE = ['https://analysis.windows.net/powerbi/api/.default']
