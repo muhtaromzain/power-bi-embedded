9a9727a485f48df00dfaf8629709d23cf36266f5
muhtaromzain <muhtaromzain@gmail.com>
2023-03-29 12:51:28 +0700

    add utils to encrypt and decrypt

--- AppOwnsData/utils.py
+++ AppOwnsData/utils.py
@@ -1,5 +1,10 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
+import json
+import binascii
+from Crypto.Cipher import AES
+from Crypto import Random
+import base64
 
 class Utils:
 
@@ -72,3 +77,46 @@ class Utils:
             return 'Authority URL is not provided in the config.py file'
         
         return None
+
+    def encrypt_data(data, passphrase):
+        """
+            Encrypt using AES-256-CBC with random/shared iv
+            'passphrase' must be in hex, generate with 'openssl rand -hex 32'
+        """
+        try:
+            key = binascii.unhexlify(passphrase)
+            pad = lambda s : s+chr(16-len(s)%16)*(16-len(s)%16)
+            iv = Random.get_random_bytes(16)
+            cipher = AES.new(key, AES.MODE_CBC, iv)
+            encrypted_64 = base64.b64encode(cipher.encrypt(pad(data).encode())).decode('ascii')
+            iv_64 = base64.b64encode(iv).decode('ascii')
+            json_data = {}
+            json_data['iv'] = iv_64
+            json_data['data'] = encrypted_64
+            clean = base64.b64encode(json.dumps(json_data).encode('ascii'))
+        except Exception as e:
+            print("Cannot encrypt datas...")
+            print(e)
+            exit(1)
+        return clean
+
+    def decrypt_data(data, passphrase):
+        """
+            Decrypt using AES-256-CBC with iv
+            'passphrase' must be in hex, generate with 'openssl rand -hex 32'
+            # https://stackoverflow.com/a/54166852/11061370
+        """
+        try:
+            unpad = lambda s : s[:-s[-1]]
+            key = binascii.unhexlify(passphrase)
+            encrypted = json.loads(base64.b64decode(data).decode('ascii'))
+            encrypted_data = base64.b64decode(encrypted['data'])
+            iv = base64.b64decode(encrypted['iv'])
+            cipher = AES.new(key, AES.MODE_CBC, iv)
+            decrypted = cipher.decrypt(encrypted_data)
+            clean = unpad(decrypted).decode('ascii').rstrip()
+        except Exception as e:
+            print("Cannot decrypt datas...")
+            print(e)
+            exit(1)
+        return clean
