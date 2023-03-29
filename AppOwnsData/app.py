d2fda46147126b8a988542bcce42da619ffc0acb
muhtaromzain <muhtaromzain@gmail.com>
2023-03-29 12:51:04 +0700

    add post method to get embedded info

--- AppOwnsData/app.py
+++ AppOwnsData/app.py
@@ -3,7 +3,7 @@
 
 from services.pbiembedservice import PbiEmbedService
 from utils import Utils
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, request, render_template, send_from_directory
 from flask_cors import CORS, cross_origin
 import json
 import os
@@ -26,6 +26,34 @@ def index():
 @cross_origin()
 def get_embed_info(workspace_id, report_id, tenant_id, client_id, client_secret):
     '''Returns report embed configuration'''
+
+    config_result = Utils.check_configuration(app, workspace_id, report_id, tenant_id, client_id, client_secret)
+    if config_result is not None:
+        return json.dumps({'errorMsg': config_result}), 500
+
+    try:
+        embed_info = PbiEmbedService().get_embed_params_for_single_report(workspace_id, report_id, tenant_id, client_id, client_secret)
+        return embed_info
+    except Exception as ex:
+        return json.dumps({'errorMsg': str(ex)}), 500
+
+@app.route('/getEmbeddedInfo', methods=['POST'])
+@cross_origin()
+def getInfo():
+    '''Returns report embed configuration'''
+
+    dataJson        = request.json
+    encryptedData   = dataJson['data']
+    
+    # decode response
+    decode          = Utils.decrypt_data(encryptedData, app.config['AES_KEY'])
+    decodeData      = json.loads(decode)
+
+    workspace_id    = decodeData['workspace_id']
+    report_id       = decodeData['report_id']
+    tenant_id       = decodeData['tenant_id']
+    client_id       = decodeData['client_id']
+    client_secret   = decodeData['client_secret']
 
     config_result = Utils.check_configuration(app, workspace_id, report_id, tenant_id, client_id, client_secret)
     if config_result is not None:
